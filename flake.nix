3994062e85cc2c901555973d38c5621cce8c3d63
hugosenari <hugosenari@gmail.com>
2022-03-12 04:29:49 -0300

    chore: re index nimnews

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = ''Immature Newsgroup NNTP server using SQLite as backend'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimnews-master".url = "path:./master";
+  inputs."nimnews-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimnews-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimnews-latest-master".url = "path:./latest-master";
+  inputs."nimnews-latest-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimnews-latest-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimnews-v0_0_0".url = "path:./v0_0_0";
+  inputs."nimnews-v0_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimnews-v0_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimnews-v0_1_0".url = "path:./v0_1_0";
+  inputs."nimnews-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimnews-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v0_0_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Immature Newsgroup NNTP server using SQLite as backend'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimnews-v0_0_0.flake = false;
+  inputs.src-nimnews-v0_0_0.owner = "mildred";
+  inputs.src-nimnews-v0_0_0.ref   = "refs/tags/v0.0.0";
+  inputs.src-nimnews-v0_0_0.repo  = "nimnews";
+  inputs.src-nimnews-v0_0_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimnews-v0_0_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimnews-v0_0_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
